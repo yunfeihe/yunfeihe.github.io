4f5abeb1f4976ec318f20f19b593d5420504d2d6
YunfeiHe <yunfei.he@qq.com>
2018-06-06 21:53:36 +0800

    .

--- ife/san/lec2.5/codes/components/checkBox.js
+++ ife/san/lec2.5/codes/components/checkBox.js
@@ -8,91 +8,105 @@ import indeterminateIcon from "../icon/indeterminate.svg";
 import indeterminateDisabledIcon from "../icon/indeterminate-disabled.svg";
 
 
-const disabledTag = `
-<svg id="_san_3039" class="sm-checkbox-icon-uncheck sm-checkbox-svg-icon " viewBox="0 0 24 24">
-                        <path id="_san_3041" d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"></path>
-                    </svg>
-`
+// template: `
+//     <label class="checkbox">
+//         <input type="checkbox" 
+//             checked = "{= checked =}"              
+//             on-change = "statusChange"
+//         >
+//         <div class="icon">
+//             <img s-if="indeterminate" src="{{ disabled  ? '${indeterminateDisabledIcon}' : '${indeterminateIcon}'}}">
+//             <img s-elif="!disabled" src="{{ checkedValues.length === 0 ? '${basicIcon}' : '${checkedIcon}'}}">
+//             <img s-else src="{{ checkedValues.length === 0 ? '${disabledIcon}' : '${checkedDisabledIcon}'}}">
+//         </div>
+//         <div s-if="debug">debug isChecked: {{ isChecked }}  checked: {{ checked }}</div>
+//     </label>
+//     `,
+
+{/* <img s-if="indeterminate" src="{{ disabled  ? '${indeterminateDisabledIcon}' : '${indeterminateIcon}'}}">
+            <img s-elif="!disabled" src="{{ checkedValues.length === 0 ? '${basicIcon}' : '${checkedIcon}'}}">
+            <img s-else src="{{ checkedValues.length === 0 ? '${disabledIcon}' : '${checkedDisabledIcon}'}}"> */}
 
 export default san.defineComponent({
-    template: `
+    template: 
+`
     <label class="checkbox">
         <input type="checkbox" 
-            checked = "{= checked =}"              
+            checked = "{{ wrapChecked }}"              
             on-change = "statusChange"
-        >
+            disabled = "{{ disabled }}"
+            style="{{ debug ? '' : 'display:none;' }}"
+        />
         <div class="icon">
-            <img s-if="indeterminate" src="{{ disabled  ? '${indeterminateDisabledIcon}' : '${indeterminateIcon}'}}">
-            <img s-elif="!disabled" src="{{ checkedValues.length === 0 ? '${basicIcon}' : '${checkedIcon}'}}">
-            <img s-else src="{{ checkedValues.length === 0 ? '${disabledIcon}' : '${checkedDisabledIcon}'}}">
-        </div>
-        <div s-if="debug">debug isChecked: {{ isChecked }}  checked: {{ checked }}</div>
-    </label>
-    `,
-    initData: function() {
-        return {
-            checked: "defalut",
-            falseValue: "",
-        };
-    },
-    dataTypes: {
-        checkedValues: san.DataTypes.arrayOf(san.DataTypes.string),
-        trueValue: san.DataTypes.string,
-        falseValue: san.DataTypes.string,
-        isChecked: san.DataTypes.bool,
-    },
-    //event method
-    debug(value) {
-        if(this.data.get("debug")) {
-            let isTrueValueSetted = this.data.get("trueValue");
+        <img s-if="indeterminate" src="{{ disabled  ? '${indeterminateDisabledIcon}' : '${indeterminateIcon}'}}">
+        <img s-elif="checked" src="{{ disabled  ? '${checkedDisabledIcon}' :  '${checkedIcon}' }}">
+        <img s-else src="{{ disabled  ? '${disabledIcon}' :  '${basicIcon}' }}">
 
-            console.log("value", value, isTrueValueSetted);
+
+        </div>
+        <div s-if="debug">debug true false value: {{ trueValue }} {{ falseValue }} checked: {{ checked }}</div>
+    </label>
+`,
+    initData() {
+        return {
+            checked: false,
+            disabled: false,
+            trueValue: '__trueValue',
+            falseValue: '__falseValue',
+            indeterminate: false,
+            debug: false,
         }
     },
+    //event func
     statusChange(e) {
-        console.log("change")
-        let isChecked = this.data.get("checked");
-        let isTrueValueSetted = this.data.get("trueValue");
-        if(isTrueValueSetted) {
-            if(false) {
-                this.data.set("checked", this.data.get("trueValue"));
-            } else {
-                this.data.set("checked", this.data.get("falseValue") || "false**");
-            }
+        let indeterminate = this.data.get('indeterminate');
+
+        const defaultTrueValue = '__trueValue';
+        let trueValue = this.data.get("trueValue");
+        let checkedValue = this.data.get("checked");
+        if(trueValue === defaultTrueValue) {
+            this.data.set("checked", !indeterminate && !checkedValue);
         } else {
-            //do nothing
-        }
-    },
-    filters: {
-        realBool(value) {
-            console.log("Test");
             let falseValue = this.data.get("falseValue");
-            if(value === falseValue) {
-                return false;
+            if(checkedValue === false) { //第一次初始化
+                checkedValue = falseValue;
+            }
+
+            if(indeterminate || checkedValue === trueValue) {
+                this.data.set("checked", falseValue);
+            } else if (!indeterminate && checkedValue === falseValue) {
+                this.data.set("checked", trueValue);
             } else {
-                return true;
+               throw new Error("您已经设置的属性trueValue的值，属性checked的值必须在trueValue和falseValue之间,现在", this.data.get("checked"));
             }
         }
+        this.fire('change', e);
+        return e;
     },
     computed: {
-        isChecked() {
-            let value = this.data.get("checked");
-            let falseValue = this.data.get("falseValue"); 
-            let isTrueValueSetted = this.data.get("trueValue");
-            if(isTrueValueSetted) {
-                //value并非纯粹布尔值，已经是用户自定义的
-                console.log("value ****", value, typeof value)
-                console.log("falseValue ****", falseValue, typeof falseValue)
-                if(value === falseValue) {
-                    //falseValue可能在js里面属于真值，要显式判断下
-                    log("asd");
-                    return false;     
-                } else {
-                    return true;
-                }
+        wrapChecked() {
+            let indeterminate = this.data.get('indeterminate');
+
+        
+            const defaultTrueValue = '__trueValue';
+            let trueValue = this.data.get("trueValue");
+            let checkedValue = this.data.get("checked");
+            if(trueValue === defaultTrueValue) {
+                return !indeterminate && checkedValue;
             } else {
-                return value;
+                let falseValue = this.data.get("falseValue");
+                if(checkedValue === false) { //第一次初始化
+                    checkedValue = falseValue;
+                } 
+                if(!indeterminate && checkedValue === trueValue) {
+                    return true;
+                } else if (indeterminate || checkedValue === falseValue) {
+                    return false;
+                } else {
+                    console.log("*", checkedValue.length, trueValue, falseValue.length);
+                    throw new Error("您已经设置的属性trueValue的值，属性checked的值必须在trueValue和falseValue之间,现在" + this.data.get("checked"));
+                }
             }
-        }
+        },
     }
-}); 
+});
--- ife/san/lec2.5/codes/index.js
+++ ife/san/lec2.5/codes/index.js
@@ -1,77 +1,128 @@
 const san = require("san");
 
 import inputBox from "./components/inputBox";
-
+import checkBox from "./components/checkBox";
 // import test from "./icon/basic.svg";
 // console.log("test", test, typeof test);
 
+
+
 let App = san.defineComponent({
     template: `
-
     <div id="app">
-         <el-input 
-            placeholder="normal" 
-        >
-        </el-input>
-        <br>
-        <el-input 
-            placeholder="请输入文字" 
-            on-input="showEvent" 
-            on-focus="showEvent" 
-            on-blur="showEvent" 
-            value="事件测试"
-        >
-        </el-input>
-        <br>
-        <el-input 
-            placeholder="禁用" 
-            disabled
-        >
-        </el-input>
-        <br>
-        <el-input 
-            placeholder="只读" 
-            readOnly
-        >
-        </el-input>
-        <br>
-        <el-input 
-            placeholder="警告 版本1" 
-            warned
-            warningType = "{{1}}";
-            warningText = "警告 版本1"
-        >
-        </el-input>
-        <br>
-        <el-input 
-            placeholder="警告 版本2" 
-            warned
-            warningType = "{{2}}";
-            warningText = "警告 版本2"
-        >
-        </el-input>
-        <br>
-        <el-input 
-            placeholder="警告 版本3" 
-            warned
-            warningType = "{{3}}";
-            warningText = "警告 版本3"
-        >
-        </el-input>
+    <el-input 
+placeholder="normal" 
+>
+</el-input>
+<br>
+<el-input 
+placeholder="请输入文字" 
+on-input="showEvent" 
+on-focus="showEvent" 
+on-blur="showEvent" 
+value="事件测试"
+>
+</el-input>
+<br>
+<el-input 
+placeholder="禁用" 
+disabled
+>
+</el-input>
+<br>
+<el-input 
+placeholder="只读" 
+readOnly
+>
+</el-input>
+<br>
+<el-input 
+placeholder="警告 版本1" 
+warned
+warningType = "{{1}}";
+warningText = "警告 版本1"
+>
+</el-input>
+<br>
+<el-input 
+placeholder="警告 版本2" 
+warned
+warningType = "{{2}}";
+warningText = "警告 版本2"
+>
+</el-input>
+<br>
+<el-input 
+placeholder="警告 版本3" 
+warned
+warningType = "{{3}}";
+warningText = "警告 版本3"
+>
+</el-input>
+        
+
+    
+    <div class="checkbox-wrap">
+        <ul>
+            <li><el-checkbox checked="{{ true }}"></el-checkbox></li>
+            <li><el-checkbox checked="{{ true }}" disabled="{{ true }}"></el-checkbox></li>
+            <li><el-checkbox indeterminate="{{ true }}" disabled="{{ true }}"></el-checkbox></li>
+            <li>
+                <el-checkbox checked = "{= checked =}" indeterminate = "{{ indeterminate }}"></el-checkbox>
+                <ul>
+                    <li><el-checkbox on-change="foo(0, $event)"></el-checkbox></li>
+                    <li><el-checkbox on-change="foo(1, $event)"></el-checkbox></li>
+                    <li><el-checkbox on-change="foo(2, $event)"></el-checkbox></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+
     </div>
     `,
     components: {
         "el-input": inputBox,
+        'el-checkbox': checkBox,
     },
     initData: function() {
         return {
-            checked: true,
+            checked: false,
+            indeterminate: false,
         };
     },
     //event method
-    showEvent(e) {
-        console.log("event: ", e);
-    }
+    log(e) {
+        console.log("event: father", e);
+        console.log("ee");
+    },
+    foo: (function() {
+        let storage = new Array(3).fill(false);
+
+        return function(index, e) {
+            storage[index] = e.target.checked;
+            let trueCount = 0;
+            let falseCount = 0;
+            for(let b of storage) {
+                if(b === false) {
+                    falseCount++;
+                } else {
+                    trueCount++;
+                }
+            }
+
+            if(trueCount === 3) {
+                this.data.set("indeterminate", false);
+                this.data.set("checked", true);
+            } else if (falseCount === 3) {
+                this.data.set("indeterminate", false);
+                this.data.set("checked", false);
+            } else {
+                this.data.set("indeterminate", true);
+            }
+              
+        }
+    })(),
+
 });
 
 window.app = new App();
